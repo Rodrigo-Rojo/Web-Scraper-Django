dd0c9f3d11ef44d4ed50d837c0194de5308ffc7b
rodrigo <sorioner@hotmail.com>
2022-03-06 17:27:43 -0700

    Added New York Times, AP hab been added to scrape data successfully.

--- mysite/scraper/views.py
+++ mysite/scraper/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import New
-from .main import update_idaho_state_journal_db, update_east_idaho_news_db, update_yahoo_news_db
+from .main import update_idaho_state_journal_db, update_east_idaho_news_db, update_yahoo_news_db, update_new_york_times_db, update_associated_press_db, update_cnn_db
 # Create your views here.
 
 
 def index(request):
-    update_idaho_state_journal_db()
-    update_yahoo_news_db()
-    update_east_idaho_news_db()
+    # update_idaho_state_journal_db()
+    # update_yahoo_news_db()
+    # update_east_idaho_news_db()
+    # update_new_york_times_db()
+    update_cnn_db()
     news = New.objects.all()
     return render(request, "scraper/home.html", {"news": news})
