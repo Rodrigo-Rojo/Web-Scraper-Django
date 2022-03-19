a9fad6d67a747afd778f7a0a930d2da47650e7ef
rodrigo <sorioner@hotmail.com>
2022-03-19 13:51:50 -0600

    apscheduler been set up and now takes care of scraping data in a daily schedule.

--- mysite/scraper/apps.py
+++ mysite/scraper/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class ScraperConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scraper'
+
+    def ready(self):
+        from scraperUpdater import updater
+
+        updater.start()
