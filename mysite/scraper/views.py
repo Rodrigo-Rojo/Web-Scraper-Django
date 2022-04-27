8a7879eff3096a5408f5a65ddedcf2d8d69831b0
rodrigo <sorioner@hotmail.com>
2022-04-26 23:46:54 -0500

    Fix scraper.py where there was some errors while scraping data. Added more data to home.html. Added and implemented data from database to views.py to render data by date and showing last 10 news in database. Added and implemented style to css.

--- mysite/scraper/views.py
+++ mysite/scraper/views.py
@@ -1,8 +1,54 @@
 from django.shortcuts import render
 from .models import New
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 
 def index(request):
-    news = New.objects.all()
-    return render(request, "scraper/home.html", {"news": news})
+    news = New.objects.all().order_by("-date")
+    ap = [new for new in news.order_by("-date") if new.site == "AssociatedPress"][:10]
+    wp = [new for new in news.order_by("-date") if new.site == "WashingtonPost"][:10]
+    sky = [new for new in news.order_by("-date") if new.site == "SkyNews"][:10]
+    cnn = [new for new in news.order_by("-date") if new.site == "CNN"][:10]
+    nyt = [new for new in news.order_by("-date") if new.site == "NewYorkTimes"][:10]
+    yh = [new for new in news.order_by("-date") if new.site == "YahooNews"][:10]
+    ein = [new for new in news.order_by("-date") if new.site == "EastIdahoNews"][:10]
+    isj = [new for new in news.order_by("-date") if new.site == "IdahoStateJournal"][:10]
+    # for new in New.objects.values_list('title', flat=True).distinct():
+    #     New.objects.filter(pk__in=New.objects.filter(title=new).values_list('id', flat=True)[1:]).delete()
+
+    # ap_paginator = Paginator([new for new in news if new.site == "AssociatedPress"][:10], 10)
+    # page_number = request.GET.get('page')
+    # ap = ap_paginator.get_page(page_number)
+    #
+    # wp_paginator = Paginator([new for new in news if new.site == "WashingtonPost"], 10)
+    # wp = wp_paginator.get_page(page_number)
+    #
+    # sky_paginator = Paginator([new for new in news if new.site == "SkyNews"], 10)
+    # sky = sky_paginator.get_page(page_number)
+    #
+    # cnn_paginator = Paginator([new for new in news if new.site == "CNN"], 10)
+    # cnn = cnn_paginator.get_page(page_number)
+    #
+    # nyt_paginator = Paginator([new for new in news if new.site == "NewYorkTimes"], 10)
+    # nyt = nyt_paginator.get_page(page_number)
+    #
+    # yh_paginator = Paginator([new for new in news if new.site == "YahooNews"], 10)
+    # yh = yh_paginator.get_page(page_number)
+    #
+    # ein_paginator = Paginator([new for new in news if new.site == "EastIdahoNews"], 10)
+    # ein = ein_paginator.get_page(page_number)
+    #
+    # isj_paginator = Paginator([new for new in news if new.site == "IdahoStateJournal"], 10)
+    # isj = isj_paginator.get_page(page_number)
+
+    return render(request, "scraper/home.html", {"ap": ap,
+                                                 "wp": wp,
+                                                 "sky": sky,
+                                                 "cnn": cnn,
+                                                 "nyt": nyt,
+                                                 "yh": yh,
+                                                 "ein": ein,
+                                                 "isj": isj
+                                                 })
--- mysite/scraperUpdater/updater.py
+++ mysite/scraperUpdater/updater.py
@@ -1,12 +1,17 @@
-from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from .scraper import *
 
 
 def start():
     scheduler = BackgroundScheduler()
-    jobs = (update_idaho_state_journal_db, update_cnn_db, update_sky_news_db, update_yahoo_news_db,
-            update_washington_post_db, update_associated_press_db, update_east_idaho_news_db, update_washington_post_db)
+    jobs = (update_idaho_state_journal_db,
+            update_cnn_db,
+            update_sky_news_db,
+            update_yahoo_news_db,
+            update_washington_post_db,
+            update_associated_press_db,
+            update_east_idaho_news_db,
+            update_washington_post_db)
     for job in jobs:
         scheduler.add_job(job, 'interval', minutes=60)
 
