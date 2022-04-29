d7925b21e9e0c59a2f5a1d8c70d5efbf2447f104
rodrigo <sorioner@hotmail.com>
2022-04-29 11:06:58 -0500

    debug set to true

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-4v26)h)@p123pq357)p8ttpluz%@4(%vm-t8y9769()=qv4+a4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
