92a9a632ff979faecdedbbe2d90197924d03924d
rodrigo <sorioner@hotmail.com>
2022-04-29 12:33:34 -0500

    added postgres as a database

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
+import django_heroku
+import psycopg2
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-4v26)h)@p123pq357)p8ttpluz%@4(%vm-t8y9769()=qv4+a4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['ddah.herokuapp.com']
 
 
 # Application definition
@@ -85,10 +87,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dfeemisv7nr5g8',
+        'USER': 'ihelapaupzcfuw',
+        'PASSWORD': 'cee8f9854b189d154f7930f1e81fb549b5841821164575b2df80b3b4faef724c',
+        'HOST': 'ec2-52-71-69-66.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -138,6 +151,7 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
